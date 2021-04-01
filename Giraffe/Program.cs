e408a10a7b3594821c73aff37d01a4fc488596c3
Saba Teklu <saab.teklu@gmail.com>
2021-03-31 21:25:45 -0600

    for loops example

--- Giraffe/Program.cs
+++ Giraffe/Program.cs
@@ -7,38 +7,11 @@ namespace Giraffe
         // only code that is run within Main, so if you want to run code you have to call it in Main
         static void Main(string[] args)
         {
-            // building a guessing game where the user has to continue guessing the word until they get it correct
-
-            string secretWord = "giraffe";
-            string guess = "";
-            int guessLimit = 3;
-            int guessCount = 0;
-            bool outOfGuesses = false;
-            
-
-            while (guess != secretWord && !outOfGuesses)
+            int[] luckyNumbers = { 4, 7, 9, 12, 4 };
+            for (int i = 0; i < luckyNumbers.Length; i++)
             {
-                if (guessCount < guessLimit)
-                {
-                    Console.Write("Enter guess: ");
-                    guess = Console.ReadLine();
-                    guessCount++;
-                }
-                else
-                {
-                    outOfGuesses = true;
-                }
-
-            }
-            if (outOfGuesses)
-            {
-                Console.WriteLine("you lose!");
-            }
-            else
-            {
-                Console.WriteLine("you win!");
+                Console.WriteLine(luckyNumbers[i]);
             }
         }
-       
     }
 }
