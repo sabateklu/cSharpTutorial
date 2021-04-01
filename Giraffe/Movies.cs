8cb51aa36da3c773b429a520287d9e791c791100
Saba Teklu <saab.teklu@gmail.com>
2021-04-01 07:40:28 -0600

    Movies Class created with private attribute

---
+++ Giraffe/Movies.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Giraffe
+{
+    public class Movies
+    {
+        public string title, producer;
+        // private makes it so only the code in the Movies class can access the rating
+        private string rating;
+        public Movies(string aTitle, string aProducer, string aRating)
+        {
+            title = aTitle;
+            producer = aProducer;
+            Rating = aRating;
+        }
+
+        public string Rating
+        {
+            get { return rating;}
+            set {
+                if(value == "G" || value == "PG" || value == "PG-13" || value == "R" || value == "NR")
+                {
+                    rating = value;
+                }
+                else
+                {
+                    rating = "NR";
+                }
+            }
+        }
+    }
+}
