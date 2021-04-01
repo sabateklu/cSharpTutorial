2e2de777882e310c6534b2ddf8a7e60fc8c13bbb
Saba Teklu <saab.teklu@gmail.com>
2021-03-31 20:18:48 -0600

    conditionals examples

--- Giraffe/Program.cs
+++ Giraffe/Program.cs
@@ -7,17 +7,22 @@ namespace Giraffe
         // only code that is run within Main, so if you want to run code you have to call it in Main
         static void Main(string[] args)
         {
-            double cubedNumber = cube(5);
-            Console.WriteLine(cubedNumber);
-        }
+            bool isFemale = false;
+            bool isTall = true;
 
-        // break down of a method: static returnType methodName
-        // when you create a method that returns a value, the returnType is the data type that you will return
-
-        static double cube(int num)
-        {
-            double result = Math.Pow(num, 3);
-            return result;
+            if (isFemale && isTall)
+            {
+                Console.WriteLine("you are female and tall");
+            } else if (isFemale && !isTall)
+            {
+                Console.WriteLine("you are a short female");
+            } else if (!isFemale && isTall)
+            {
+                Console.WriteLine("you are not female and you're tall");
+            } else
+            {
+                Console.WriteLine("you are neither a female or tall");
+            }
         }
     }
 }
