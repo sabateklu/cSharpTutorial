8317e0cf01a5d70f5fa7e3843021f9eac60d48e2
Saba Teklu <saab.teklu@gmail.com>
2021-03-31 21:36:38 -0600

    multi-dimensional arrays

--- Giraffe/Program.cs
+++ Giraffe/Program.cs
@@ -7,18 +7,38 @@ namespace Giraffe
         // only code that is run within Main, so if you want to run code you have to call it in Main
         static void Main(string[] args)
         {
-            Console.WriteLine(GetPow(3, 3));
-        }
-        // create method that takes two positive integer arguments and returns the first argument to the power of the second argument
-        static int GetPow(int baseNum, int powNum)
-        {
-            int result = 1;
-
-            for (int i = 0; i < powNum; i++)
+            //2d arrays
+            int[,] numberGrid =
             {
-                result *= baseNum;
-            }
-            return result;
+                {1, 2 },
+                {3, 4 },
+                {5, 6 },
+            };
+            // array with two rows, and 3 colums
+            int[,] myArray = new int[2, 3];
+
+            // 3d array
+            int[,,] threeDee =
+            {
+                {
+                    {5,6 },
+                    {7,8 }
+                },
+                {
+                    {9,10 },
+                    {11, 12 }
+                },
+                {
+                    {13,14 },
+                    {15,16 }
+                }
+            };
+
+            Console.WriteLine(threeDee[0, 0, 0]); // prints 5
+            Console.WriteLine(threeDee[2, 1, 0]); // prints 15
+            Console.WriteLine(numberGrid[0, 0]); // prints 1
+            Console.WriteLine(numberGrid[1, 1]); // prints 4
         }
+        
     }
 }
