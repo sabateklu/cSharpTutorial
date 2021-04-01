fe6f865cf2e319ce529b2ce358f7eb5bb4261060
Saba Teklu <saab.teklu@gmail.com>
2021-03-31 21:30:45 -0600

    GetPow method creation

--- Giraffe/Program.cs
+++ Giraffe/Program.cs
@@ -7,11 +7,18 @@ namespace Giraffe
         // only code that is run within Main, so if you want to run code you have to call it in Main
         static void Main(string[] args)
         {
-            int[] luckyNumbers = { 4, 7, 9, 12, 4 };
-            for (int i = 0; i < luckyNumbers.Length; i++)
+            Console.WriteLine(GetPow(3, 3));
+        }
+        // create method that takes two positive integer arguments and returns the first argument to the power of the second argument
+        static int GetPow(int baseNum, int powNum)
+        {
+            int result = 1;
+
+            for (int i = 0; i < powNum; i++)
             {
-                Console.WriteLine(luckyNumbers[i]);
+                result *= baseNum;
             }
+            return result;
         }
     }
 }
