f1adcdd0ef40544b49390f88f2635f169979c15f
Saba Teklu <saab.teklu@gmail.com>
2021-04-01 07:52:48 -0600

    Song class created with static attribute songCount

---
+++ Giraffe/Song.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Giraffe
+{
+    public class Song
+    {
+        public string title;
+        public string artist;
+        public int duration;
+
+        public static int songCount = 0;
+
+        public Song(string aTitle, string aArtist, int aDuration)
+        {
+            title = aTitle;
+            artist = aArtist;
+            duration = aDuration;
+
+            songCount++;
+        }
+        // method created to access static attribute via object instance
+        public int getSongCount()
+        {
+            return songCount;
+        }
+    }
+}
