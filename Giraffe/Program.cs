4d9fad2c953ef1adb7b3b54f50dd847e8a341b03
Saba Teklu <saab.teklu@gmail.com>
2021-03-31 20:07:47 -0600

    return methods

--- Giraffe/Program.cs
+++ Giraffe/Program.cs
@@ -7,16 +7,17 @@ namespace Giraffe
         // only code that is run within Main, so if you want to run code you have to call it in Main
         static void Main(string[] args)
         {
-            SayHi("Saba", 28);
-            SayHi("Axum", 29);
+            double cubedNumber = cube(5);
+            Console.WriteLine(cubedNumber);
         }
 
-        // create a new method that says hi to the user;
         // break down of a method: static returnType methodName
+        // when you create a method that returns a value, the returnType is the data type that you will return
 
-        static void SayHi(string name, int age)
+        static double cube(int num)
         {
-            Console.WriteLine("Hello " + name + ", you are " + age + " years old");
+            double result = Math.Pow(num, 3);
+            return result;
         }
     }
 }
