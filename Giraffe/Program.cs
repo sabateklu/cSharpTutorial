3772ce38e4a33ef670b3d2fdf9038b95715829b8
Saba Teklu <saab.teklu@gmail.com>
2021-03-31 20:47:22 -0600

    while loops and do while loops

--- Giraffe/Program.cs
+++ Giraffe/Program.cs
@@ -7,42 +7,16 @@ namespace Giraffe
         // only code that is run within Main, so if you want to run code you have to call it in Main
         static void Main(string[] args)
         {
-            Console.WriteLine(GetDay(80));
-        }
-        // create a method that takes a number and returns the correlating day of the week
-       static string GetDay(int dayNum)
-        {
-            string dayName;
+            int index = 6;
 
-            switch (dayNum)
+            do // executes the code in the block before checking the condition
             {
-                case 0:
-                    dayName = "Sunday";
-                    break;
-                case 1:
-                    dayName = "Monday";
-                    break;
-                case 2:
-                    dayName = "Tuesday";
-                    break;
-                case 3:
-                    dayName = "Wednesday";
-                    break;
-                case 4:
-                    dayName = "Thursday";
-                    break;
-                case 5:
-                    dayName = "Friday";
-                    break;
-                case 6:
-                    dayName = "Saturday";
-                    break;
-                default:
-                    dayName = "Invalid day number";
-                    break;
-            }
+                Console.WriteLine(index);
+                index++;
+            } while (index <= 5) ;
 
-            return dayName;
+
         }
+       
     }
 }
