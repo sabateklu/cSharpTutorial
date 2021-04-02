ac37ebd48f53674d42ec5271b8a9a008279b045a
Saba Teklu <saab.teklu@gmail.com>
2021-04-02 11:51:22 -0600

    use of Guid data type and getters/setters

---
+++ Giraffe/Testing.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Giraffe
+{
+    public class Testing
+    {
+        private Guid id;
+
+        public Testing (Guid aId)
+        {
+            Id = aId;
+        }
+        
+        public Guid Id
+        {
+            get{ return id;}
+            set
+            {
+                id = value;
+            }
+        }
+    }
+}
